f363595ab8d480dd07e6bc10d55bcfe18513817a
nikolyasanych <Shaposhnikovnick@yandex.ru>
2023-10-01 14:24:50 +0500

    1.5

--- src/main/java/ru/calculator/Calculator.java
+++ src/main/java/ru/calculator/Calculator.java
@@ -1,19 +1,14 @@
 package ru.calculator;
 
 public class Calculator {
-    public static void main(String[] args) {
-        int six = 6;
-        int four = 4;
-        int five = 5;
-        int sixDivTwo = six / 2;
-        int fiveMinusTwo = five - 2;
-        int fourTimeTwo = four * 2;
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
 
-        int size = 10;
-        size = 100;
-        size -= 5;
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        Calculator.plus(2, 3);
+        Calculator.plus(100, 500);
     }
 }
