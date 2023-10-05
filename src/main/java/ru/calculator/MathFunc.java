e1b63592f04b63692e5e560ccd1ead727a483a9f
nikolyasanych <Shaposhnikovnick@yandex.ru>
2023-10-05 20:25:53 +0500

    1.7 Результат работы метода ДЗ

---
+++ src/main/java/ru/calculator/MathFunc.java
@@ -0,0 +1,19 @@
+package ru.calculator;
+
+public class MathFunc {
+
+    public static int func1(int x) {
+        int y = x * x + 1;
+        return y;
+    }
+
+    public static double func2(double x) {
+        double y = 1 / x;
+        return y;
+    }
+
+    public static void main(String[] args) {
+        int result3 = MathFunc.func1(100);
+        System.out.println(result3);
+    }
+}
