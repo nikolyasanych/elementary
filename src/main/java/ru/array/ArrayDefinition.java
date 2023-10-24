aa854c5bab904d7e13c52c740ae96a2e59b06f08
nikolyasanych <Shaposhnikovnick@yandex.ru>
2023-10-24 20:14:35 +0500

    5.3

--- src/main/java/ru/array/ArrayDefinition.java
+++ src/main/java/ru/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+        String[] names = new String[4];
+        names[0] = "Ivan";
+        names[1] = "Kolya";
+        names[2] = "Artem";
+        names[3] = "Katya";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
