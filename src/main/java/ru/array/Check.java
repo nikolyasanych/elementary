5b0e25984501a6d5329431c5440f4122bc161d1c
nikolyasanych <Shaposhnikovnick@yandex.ru>
2023-10-26 19:01:51 +0500

    5.13 массив заполнен true или false ДЗ

---
+++ src/main/java/ru/array/Check.java
@@ -0,0 +1,16 @@
+package ru.array;
+
+public class Check {
+    public static boolean mono(boolean[] data) {
+        boolean result = true;
+        boolean temp = data[0];
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] != temp) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+}
