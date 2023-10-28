1ec04ecc7ae27d4fee827f87ef493a1992f4197f
nikolyasanych <Shaposhnikovnick@yandex.ru>
2023-10-28 16:25:06 +0500

    5.14 fix

--- src/main/java/ru/array/Check.java
+++ src/main/java/ru/array/Check.java
@@ -4,7 +4,7 @@ public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = true;
         boolean temp = data[0];
-        for (int i = 0; i < data.length; i++) {
+        for (int i = 1; i < data.length; i++) {
             if (data[i] != temp) {
                 result = false;
                 break;
