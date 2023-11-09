0f24262cae522ee87399170acf68a060a155dd3b
nikolyasanych <Shaposhnikovnick@yandex.ru>
2023-11-09 16:49:04 +0500

    5.20 Поиск минимума в диапазоне ДЗ

---
+++ src/main/java/ru/array/MinDiapason.java
@@ -0,0 +1,14 @@
+package ru.array;
+
+public class MinDiapason {
+    public static int findMin(int[] array, int start, int finish) {
+        int min = array[start];
+        for (int index = start; index <= finish; index++) {
+            if (array[index] < min) {
+                min = array[index];
+            }
+        }
+        return min;
+    }
+
+}
