05005a063d5000be54c8d178a98a0a7c70c6a1df
nikolyasanych <Shaposhnikovnick@yandex.ru>
2023-11-10 22:36:37 +0500

    5.23

--- src/main/java/ru/array/MinDiapason.java
+++ src/main/java/ru/array/MinDiapason.java
@@ -3,7 +3,7 @@ package ru.array;
 public class MinDiapason {
     public static int findMin(int[] array, int start, int finish) {
         int min = array[start];
-        for (int index = start; index <= finish; index++) {
+        for (int index = start + 1; index <= finish; index++) {
             if (array[index] < min) {
                 min = array[index];
             }
