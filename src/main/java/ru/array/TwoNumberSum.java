be4ab723316e1e51c91dd9d6bc2b8ad9176b7013
nikolyasanych <Shaposhnikovnick@yandex.ru>
2023-11-15 12:35:31 +0500

    5.31 исправление ДЗ

--- src/main/java/ru/array/TwoNumberSum.java
+++ src/main/java/ru/array/TwoNumberSum.java
@@ -4,14 +4,15 @@ public class TwoNumberSum {
     public static int[] getIndexes(int[] array, int target) {
         int i = 0;
         int j = array.length - 1;
-        while (i != j) {
+        while (i < j) {
             int sum = array[i] + array[j];
-            if (sum > target) {
-                j--;
-            } else if (sum == target) {
+            if (sum == target) {
                 return new int[]{i, j};
+            } else if (sum > target) {
+                j--;
+            } else {
+                i++;
             }
-            i++;
         }
         return new int[0];
     }
