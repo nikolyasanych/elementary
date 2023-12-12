69f3d4b3ad278f959ee124b8cb59c4a1452b8da9
nikolyasanych <Shaposhnikovnick@yandex.ru>
2023-12-12 16:59:56 +0500

    7.15 Перегрузить метод max для трех чисел ДЗ

--- src/main/java/ru/condition/Max.java
+++ src/main/java/ru/condition/Max.java
@@ -6,6 +6,16 @@ public class Max {
         return result;
     }
 
+    public static int max(int left, int right, int center) {
+        int result = max(left, center) > right ? max(left, center) : right;
+        return result;
+    }
+
+    public static int max(int left, int right, int center, int middle) {
+        int result = max(left, center) > max(right, middle) ? max(left, center) : max(right, middle);
+        return result;
+    }
+
     public static void main(String[] args) {
         int maximum = Max.max(2, 10);
         System.out.println(maximum);
--- src/test/java/ru/condition/MaxTest.java
+++ src/test/java/ru/condition/MaxTest.java
@@ -42,4 +42,25 @@ class MaxTest {
         assertThat(result).isEqualTo(expected);
     }
 
+    @Test
+    void whenMax2To3To4Then4() {
+        int left = 2;
+        int right = 3;
+        int center = 4;
+        int result = Max.max(left, right, center);
+        int expected = 4;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax2To3To5To10Then10() {
+        int left = 2;
+        int right = 3;
+        int center = 5;
+        int middle = 10;
+        int result = Max.max(left, right, center, middle);
+        int expected = 10;
+        assertThat(result).isEqualTo(expected);
+    }
+
 }
