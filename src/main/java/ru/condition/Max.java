379f7e9cd995429bdfa82404025977953e1bc875
nikolyasanych <Shaposhnikovnick@yandex.ru>
2023-12-13 16:29:14 +0500

    7.15 ДЗ

--- src/main/java/ru/condition/Max.java
+++ src/main/java/ru/condition/Max.java
@@ -7,13 +7,11 @@ public class Max {
     }
 
     public static int max(int left, int right, int center) {
-        int result = max(left, center) > right ? max(left, center) : right;
-        return result;
+        return max(left, center) > right ? max(left, center) : right;
     }
 
     public static int max(int left, int right, int center, int middle) {
-        int result = max(left, center) > max(right, middle) ? max(left, center) : max(right, middle);
-        return result;
+        return max(left, center) > max(right, middle) ? max(left, center) : max(right, middle);
     }
 
     public static void main(String[] args) {
